5d2d470af7e5101041f04baed177a87146244b0f
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-09-17 16:15:07 +0500

    Add implementation of Person class with various attributes

---
+++ private.cpp
@@ -0,0 +1,33 @@
+#include "iostream"
+
+using namespace std;
+
+class Person{
+    int id;
+    string name;
+    string fathername;
+    int age;
+    double phone_no;
+    double cnic;
+    string address;
+    string gender;
+    float height;
+    float weight;
+    string dob;
+    string marital_status;
+    int siblings;
+    string education;
+    string bank_info;
+    string nationality;
+    string religion;
+    string email;
+    string birthmark;
+    string speciality;
+    string blood_group;
+
+};
+
+int main()
+{
+
+}
