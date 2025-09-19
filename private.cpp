abbb7c7fabc0c8f8652670c71eb838c854613d3d
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-09-19 14:00:17 +0500

    Refactor Person class to organize member variables and improve encapsulation

--- private.cpp
+++ private.cpp
@@ -2,32 +2,34 @@
 
 using namespace std;
 
-class Person{
-    int id;
-    string name;
+class Person
+{
+private:
     string fathername;
     int age;
     double phone_no;
     double cnic;
     string address;
-    string gender;
     float height;
     float weight;
-    string dob;
     string marital_status;
     int siblings;
     string education;
     string bank_info;
-    string nationality;
-    string religion;
     string email;
     string birthmark;
-    string speciality;
-    string blood_group;
 
+public:
+    int id;
+    string name;        // public
+    string speciality;  // public
+    string blood_group; // public
+    string nationality; // public
+    string religion;    // public
+    string gender;      // public
+    string dob;         // public
 };
 
 int main()
 {
-
 }
