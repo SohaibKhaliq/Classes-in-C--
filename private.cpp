c7f9161ebf0fb61a0b2ac10ea7fcd6c44e69a6d3
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-09-19 14:57:56 +0500

    Refactor Person class constructor and improve member variable organization

--- private.cpp
+++ private.cpp
@@ -20,16 +20,28 @@ private:
     string birthmark;
 
 public:
+    Person()
+    {
+        cout << "Constructor is Called" <<endl;
+        name="sohaib";
+        blood_group="A+";
+    }
     int id;
-    string name;        
-    string speciality;  
-    string blood_group; 
-    string nationality; 
-    string religion;    
-    string gender;      
-    string dob;         
+    string name;
+    string speciality;
+    string blood_group;
+    string nationality;
+    string religion;
+    string gender;
+    string dob;
 };
 
 int main()
 {
+    Person p1;
+    cout << p1.name << endl;
+    cout << p1.blood_group;
+    Person p2;
+    Person p3;
+    Person p4;
 }
